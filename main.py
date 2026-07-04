6e03ae676cf6a1b2f7089b80875a4ece7e15610b
LF <lucafiaccadori10@gmail.com>
2026-07-04 17:58:40 +0200

    _valutatore_kfold terminata scrittura funzione

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Dai dati presenti nella zip del dataset ho scoperto che solo una delle 3 classi 
 utilizzare principalmente metodi non lineari
 
 '''
+LABLES = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+
 from pathlib import Path
 dataset_path = Path(r"iris.csv")
 
@@ -15,7 +17,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.base import clone
 
 # sklearn lib - models
 from sklearn.neighbors import KNeighborsClassifier
@@ -82,11 +84,18 @@ print(f"distribbuzione delle classi in y_test:\n{_test_classi(y_test)}")
 Lo scalamento deve essere fatto interno al k-fold altrimenti non è attendibile valutare ogni ottimizzazione con il 
 Validator
 StratifiedKFold si preoccupa di generare per ogni fold un Validator diverso e omogeneo
-'''
-############################# Cross validation - Ottimizzazione iperparametri  #########################
-model = KNeighborsClassifier(n_neighbors= 5)
 
-def _valutatore_kfold(modello: object, X: np.array, y: np.array, verbose: bool = False, splits: int = 15, scalamento: bool = False):
+Prima del Cross validation deve essere definito un modello con i suoi iperparametri:
+Prima si sceglie il metodo di ottimizzazione degli iperparametri e poi si usa "_valutatore_kfold"  per
+testarne le metriche e scegliere il migliore.
+
+'''
+
+
+############################# Cross validation - Ottimizzazione iperparametri  #########################
+modello = KNeighborsClassifier(n_neighbors= 1)
+
+def _valutatore_kfold(modello: object, X: np.ndarray, y: np.ndarray, verbose: bool = False, splits: int = 7, scalamento: bool = False):
     '''
     Questa funzione accetta un modello (sklearn) con i suoi iperparametri e un training set.
     Sfrutta la funzione StratifiedKFold per valutare le sue prestazioni su k fold omogeneamente suddivisi.
@@ -99,10 +108,10 @@ def _valutatore_kfold(modello: object, X: np.array, y: np.array, verbose: bool =
         es:
             modello = KNeighborsClassifier(n_neighbors= 5, ...)
 
-    X : np.array
+    X : np.ndarray
         features del dataset di training da suddividere
 
-    y : np.array
+    y : np.ndarray
         lable del dataset di training da suddividere    
     
     verbose : bool
@@ -117,8 +126,8 @@ def _valutatore_kfold(modello: object, X: np.array, y: np.array, verbose: bool =
 
     returns
     -----
-    d : dict 
-        dizionario contenente le valutazioni finali del modello
+    medie : dict 
+        dizionario contenente le medie delle valutazioni finali del modello
 
     note
     -----
@@ -127,38 +136,69 @@ def _valutatore_kfold(modello: object, X: np.array, y: np.array, verbose: bool =
 
     '''
     # random_state=7 per rendere il risultato riproducibile
-    skf = StratifiedKFold(n_splits=splits, random_state=7)
+    splits = max(2, splits) # uno split minore di 2 non ha senso
+    skf = StratifiedKFold(n_splits=splits, random_state=7, shuffle=True)
+
+    valutazioni = []    # list[dict] valutazioni di ogni fold
 
     for index, (train_index, test_index) in enumerate(skf.split(X, y)):
+        
+        # non posso lavorare sulla copia originale del modello passato
+        modello_fold = clone(modello)
+        '''DEBUG
         if verbose:
             print(f"Fold {index}:")
             print(f"  Train: index={train_index}")
             print(f"  Test:  index={test_index}")
-
+        '''
+            
         # Dati per ogni fold:
         X_train = X[train_index]
-        X_validate = X[test_index]
+        X_valid = X[test_index]
 
         y_train = y[train_index]
-        y_validate = y[test_index]
+        y_valid = y[test_index]
 
         if scalamento:
             # scalamento dei dati basato sui soli dati di training
             scaler = StandardScaler()
             scaler.fit(X_train)
             X_train = scaler.transform(X_train)
-            X_validate = scaler.transform(X_validate)
+            X_valid = scaler.transform(X_valid)
 
-        modello.fit(X_train, y_train)
-        y_pred = modello.predict(X_validate)
-
-
-        # alleno il modello
-        modello.fit(...)
+        # alleno il modello e interpreto i dati di validation
+        modello_fold.fit(X_train, y_train)
+        y_pred = modello_fold.predict(X_valid)
 
 
+        # METRICHE !!!
+        valutazioni_fold={
+            'Accuracy': accuracy_score(y_valid, y_pred),
+            'Precision': precision_score(y_valid, y_pred, average='macro', zero_division=0),
+            'Recall': recall_score(y_valid, y_pred, average='macro'),
+            'F1-Score': f1_score(y_valid, y_pred, average='macro')
+        }
+        if verbose: print(f"valutazioni fold {index}\n{valutazioni_fold}")
+        valutazioni.append(valutazioni_fold)
 
+    if not valutazioni: return {}   #se vuoto esci
 
+    somme = {}
+    
+    # Somma tutte le metriche
+    for fold in valutazioni:
+        for metrica, valore in fold.items():
+            if metrica not in somme.keys(): somme[metrica]=0
+            somme[metrica] += valore
+    
+    # Calcola la media per ogni metrica
+    n_fold = len(valutazioni)
+    medie = {metrica: somma / n_fold for metrica, somma in somme.items()}
+
+    if verbose: print(f"medie valutazioni modello:\n{medie}")
+    return medie
+
+_valutatore_kfold(modello, X_train, y_train, verbose = True)
 
 
 
