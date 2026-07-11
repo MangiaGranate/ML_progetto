1228a209d58814686eaf8a395f9e31604f897a22
LF <lucafiaccadori10@gmail.com>
2026-07-11 15:12:51 +0200

    classificazione binaria

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ con un modello 1-vs-all
 
 '''
 LABLES = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+TRAT = 120      # quanti trattini mettere come separatore ad ogni sezione
+
 
 from pathlib import Path
 dataset_path = Path(r"iris.csv")
@@ -40,38 +42,34 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ############################# EDA  #########################
 
+string = '''
+#############################
+EDA
+#############################
+'''
+print(string)
+
+
 dataset = pd.read_csv(dataset_path, sep=',')
 
 # Vedere se ci sono colonne con valori nulli
 print("Controllo valori nulli:")
 print(f"valori nulli:\n{dataset.isnull().any()}")
 
-print('-'*20)   # separatore
+print('='*TRAT + '\n')   # separatore
 
 # Vedere il numero di esempi per classi
 conteggio_per_classe = dataset['y'].value_counts().sort_index()
 print("Controllo conteggio classi:")
 print(conteggio_per_classe)
 
-print('-'*20)   # separatore
+print('='*TRAT + '\n')   # separatore
 
 # Trasformo in np.array
 y = dataset.y.to_numpy()
 X = dataset.drop(columns='y').to_numpy()
 
-# Etichette per i classificatori lineari
-y_bin = {}
-for lable in LABLES:
-    y_bin[lable] = [0] * len(y)
-    for i, ele in enumerate(y):
-        if ele==lable:
-            y_bin[lable][i] = 1
-        else:
-            y_bin[lable][i] = 0
 
-y_setosa = y_bin['Iris-setosa']
-y_versicolor = y_bin['Iris-versicolor']
-Y_virginia = y_bin['Iris-virginica']
 
 
 '''
@@ -82,6 +80,7 @@ random_size=int in modo che ad ogni iterazione la funzione produca sempre lo ste
 stratify=y in modo che i due sottoinsiemi siano omogenei rispetto ad y
 '''
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.30, random_state=7, stratify=y)
+
 
 #testiamo che la funzione abbia effettivamente diviso i sample in modo omogeneo:
 
@@ -98,8 +97,24 @@ def _test_classi(array: np.array):
     return classi
 
 print(f"distribuzione delle classi in y_train:\n{_test_classi(y_train)}")
-print('-'*20)   # separatore
+print('='*TRAT + '\n')   # separatore
 print(f"distribuzione delle classi in y_test:\n{_test_classi(y_test)}")
+
+
+# Etichette per i classificatori binari
+
+y_bin = {}
+for lable in LABLES:
+    y_bin[lable] = [0] * len(y_train)
+    for i, ele in enumerate(y_train):
+        if ele==lable:
+            y_bin[lable][i] = 1
+        else:
+            y_bin[lable][i] = 0
+
+y_setosa = y_bin['Iris-setosa']
+y_versicolor = y_bin['Iris-versicolor']
+y_virginia = y_bin['Iris-virginica']
 
 
 '''
@@ -123,6 +138,8 @@ training di quel fold specifico.
 '''
 
 
+
+############################# Cross validation - Ottimizzazione iperparametri  #########################
 string = '''
 #############################
 INIZIO CROSS VALIDATION
@@ -130,7 +147,6 @@ INIZIO CROSS VALIDATION
 '''
 print(string)
 
-############################# Cross validation - Ottimizzazione iperparametri  #########################
 
     # Nearest neighbors - multiclass
 
@@ -158,10 +174,10 @@ gs = GridSearchCV(
 
 gs.fit(X_train, y_train)
 nome = 'K-Nearest Neighbor'
-print(nome + '-'*(40-len(nome)))
+print(nome + '-'*max(0,(TRAT-len(nome))))
 print('iperparametri migliori: ', gs.best_params_)
 print('accuratezza del modello:', gs.best_score_)
-print('-'*20 + '\n')   # separatore
+print('='*TRAT + '\n')   # separatore
 
 
 
@@ -188,13 +204,19 @@ gs = GridSearchCV(
 
 gs.fit(X_train, y_train)
 nome = 'Decision Tree'
-print(nome + '-'*(40-len(nome)))
+print(nome + '-'*max(0,(TRAT-len(nome))))
 print('iperparametri migliori: ', gs.best_params_)
 print('accuratezza del modello:', gs.best_score_)
-print('-'*20 + '\n')   # separatore
+print('='*TRAT + '\n')   # separatore
+
+
 
 
     # LogisticRegression - classificatori binari
+'''
+Alcune combinazioni di parametri della LogReg che la funzione "GridSearchCV" testa non sono
+valide per questo a run time alcuni fold non producono risultati validi !
+'''
 
 iperparametri = {
     'modello__C': [0.001, 0.01, 0.1, 1, 10, 100],          # Regolarizzazione
@@ -218,13 +240,21 @@ gs = GridSearchCV(
     cv=5,
     verbose=True)
 
+'''
+CLASSIFICATORI BINARI: LogReg e SVC devono essere ottimizzati per ognuna delle 3 classi; dopo la
+definizione di GridSearchCV() deve essere chiamata con ogni etichetta.
+Non posso usare la y_train che contiene più valori ma y_bin che contiene solo 1 o 0 a seconda 
+dell'appartenenza del sample alla classe positiva
+'''
 
-gs.fit(X_train, y_train)
-nome = 'LogisticRegression'
-print(nome + '-'*(40-len(nome)))
-print('iperparametri migliori: ', gs.best_params_)
-print('accuratezza del modello:', gs.best_score_)
-print('-'*20 + '\n')   # separatore
+for etichetta in LABLES :
+
+    gs.fit(X_train, y_bin[etichetta])
+    nome = f'LogisticRegression({etichetta})'
+    print(nome + '-'*max(0,(TRAT-len(nome))))
+    print('iperparametri migliori: ', gs.best_params_)
+    print('accuratezza del modello:', gs.best_score_)
+    print('='*TRAT + '\n')   # separatore  
 
 
 
@@ -233,12 +263,8 @@ print('-'*20 + '\n')   # separatore
 
 iperparametri = {
     'modello__C': [0.1, 1, 10, 100],                    # Regolarizzazione
-    'modello__kernel': ['rbf', 'linear', 'poly'],       # Tipo di kernel
+    'modello__kernel': ['rbf', 'linear'],               # Tipo di kernel
     'modello__gamma': ['scale', 'auto', 0.01, 0.1, 1],  # Coefficiente per kernel rbf/poly
-    'modello__degree': [2, 3, 4],                       # Grado per kernel polinomiale
-    'modello__class_weight': [None, 'balanced'],        # Gestione classi sbilanciate
-    'modello__probability': [True],                     # Abilita predict_proba()
-    'modello__decision_function_shape': ['ovr']         # One-vs-Rest per multiclasse
 }
 
 pipeline = Pipeline([
@@ -254,12 +280,14 @@ gs = GridSearchCV(
     verbose=True)
 
 
-gs.fit(X_train, y_train)
-nome = 'SVC'
-print(nome + '-'*(40-len(nome)))
-print('iperparametri migliori: ', gs.best_params_)
-print('accuratezza del modello:', gs.best_score_)
-print('-'*20 + '\n')   # separatore
+for etichetta in LABLES :
+
+    gs.fit(X_train, y_bin[etichetta])
+    nome = f'SVC({etichetta})'
+    print(nome + '-'*max(0,(TRAT-len(nome))))
+    print('iperparametri migliori: ', gs.best_params_)
+    print('accuratezza del modello:', gs.best_score_)
+    print('='*TRAT + '\n')   # separatore
 
 ############################# Considerazioni - Model selection  #########################
 
@@ -275,20 +303,82 @@ parametri = {
 best_model=KNeighborsClassifier(n_neighbors=5)
 best_model=DecisionTreeClassifier(criterion='entropy')
 
-def test(best_model):
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train_scal = scaler.transform(X_train)
-    X_test_scal = scaler.transform(X_test)
 
-    best_model.fit(X_train_scal, y_train)
-    y_pred = best_model.predict(X_test_scal)
+
 
 ############################# Testing  #########################
 
-    accuracy_score, precision_score, recall_score, f1_score
-    accuratezza = accuracy_score(y_test, y_pred)
-    print(f"l'accurazetta del modello {best_model} è di: {accuratezza}")
+string = '''
+#############################
+INIZIO FASE DI TESTING
+#############################
+'''
+print(string)
 
-test(best_model)
 
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train_scal = scaler.transform(X_train)
+X_test_scal = scaler.transform(X_test)
+
+best_model.fit(X_train_scal, y_train)
+y_pred = best_model.predict(X_test_scal)
+
+accuratezza = accuracy_score(y_test, y_pred)
+print(f"l'accurazetta del modello {best_model} è di: {accuratezza}")
+
+
+'''
+Testo i due classificatori binari per setosa e virginia per trovare la classe che è
+linearmente separabile
+
+
+# TESTING DI LOGREG(Iris-setosa)
+
+print('='*TRAT + '\n')   # separatore
+
+iperparametri_LogReg_setosa = {'C': 0.001, 'class_weight': None, 'max_iter': 1000, 'penalty': 'l2', 'solver': 'liblinear', 'tol': 0.0001}
+
+modello_setosa = LogisticRegression(**iperparametri_LogReg_setosa)
+
+modello_setosa.fit(X_train_scal, y_setosa)
+y_pred = modello_setosa.predict(X_test_scal)
+
+y_test_setosa = []
+
+# trasformo y_test in 0 o 1 a seconda dell'appartenenza a Iris-setosa
+y_test_setosa = []
+for i, ele in enumerate(y_test):
+    if ele=='Iris-setosa':
+        y_test_setosa.append(1)
+    else:
+        y_test_setosa.append(0)
+
+accuratezza = accuracy_score(y_test_setosa, y_pred)
+print(f"l'accurazetta del modello {modello_setosa} è di: {accuratezza}")
+
+
+
+# TESTING DI LOGREG(Iris-virginia)
+
+print('='*TRAT + '\n')   # separatore
+
+iperparametri_LogReg_virginia = {'C': 10, 'class_weight': None, 'max_iter': 1000, 'penalty': 'l2', 'solver': 'saga', 'tol': 0.0001}
+modello_virginia = LogisticRegression(**iperparametri_LogReg_virginia)
+
+modello_virginia.fit(X_train_scal, y_virginia)
+y_pred = modello_virginia.predict(X_test_scal)
+
+# trasformo y_test in 0 o 1 a seconda dell'appartenenza a Iris-virginia
+y_test_virginia = []
+for i, ele in enumerate(y_test):
+    if ele=='Iris-virginia':
+        y_test_virginia.append(1)
+    else:
+        y_test_virginia.append(0)
+
+accuratezza = accuracy_score(y_test_virginia, y_pred)
+print(f"l'accurazetta del modello {modello_virginia} è di: {accuratezza}")
+
+
+'''
